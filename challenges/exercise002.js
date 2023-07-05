bf5b8a7a4d7888ab4c27ce12768c26bf0a35926d
mousumi-ish <baral_mousumi@yahoo.com>
2023-07-05 11:37:28 +0100

    Completed exercise002

--- challenges/exercise002.js
+++ challenges/exercise002.js
@@ -1,24 +1,32 @@
 export function getFillings(sandwich) {
-	if (sandwich === undefined) throw new Error('ingredients is required');
-	// Your code here!
+  if (sandwich === undefined) throw new Error("ingredients is required");
+  return sandwich.fillings;
 }
 
 export function isFromManchester(person) {
-	if (person === undefined) throw new Error('person is required');
-	// Your code here!
+  if (person === undefined) throw new Error("person is required");
+  return person.city === "Manchester";
 }
 
 export function getBusNumbers(people) {
-	if (people === undefined) throw new Error('people is required');
-	// Your code here!
+  if (people === undefined) throw new Error("people is required");
+  const peopleInOneBus = 40;
+  return Math.ceil(people / peopleInOneBus);
 }
 
 export function countSheep(arr) {
-	if (arr === undefined) throw new Error('arr is required');
-	// Your code here!
+  if (arr === undefined) throw new Error("arr is required");
+  let animalInFarm = "sheep";
+  let count = 0;
+  for (let i = 0; i < arr.length; count += Number(arr[i++] === animalInFarm));
+  return count;
+  return arr.filter((animalInFarm) => animalInFarm === "sheep").length;
 }
 
 export function hasMPostCode(person) {
-	if (person === undefined) throw new Error('person is required');
-	// Your code here!
+  if (person === undefined) throw new Error("person is required");
+  return (
+    person.address.postCode.charAt(0) === "M" &&
+    person.address.city === "Manchester"
+  );
 }
