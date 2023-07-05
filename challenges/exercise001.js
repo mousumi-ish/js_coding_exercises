517e1d3f93a9085f9d468e841b0fc9eef8ef8ce5
mousumi-ish <baral_mousumi@yahoo.com>
2023-07-05 06:55:22 +0100

    completed getSalePrice exercise001

--- challenges/exercise001.js
+++ challenges/exercise001.js
@@ -25,7 +25,9 @@ export function addVAT(originalPrice, vatRate) {
 export function getSalePrice(originalPrice, reduction) {
   if (originalPrice === undefined) throw new Error("originalPrice is required");
   if (reduction === undefined) throw new Error("reduction is required");
-  // Add your code here!
+  return (
+    Math.round((originalPrice - (originalPrice * reduction) / 100) * 100) / 100
+  );
 }
 
 export function getMiddleCharacter(str) {
